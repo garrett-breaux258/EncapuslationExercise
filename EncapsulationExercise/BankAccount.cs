95d1dcd77f6b14995d777ca63760a6d97edc4508
Garrett Breaux <garrett.breaux1@gmail.com>
2021-07-09 23:38:50 -0500

    Bank Account

---
+++ EncapsulationExercise/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace EncapsulationExercise
+{
+    public class BankAccount
+    {
+        
+        private double balance;
+
+       
+        public BankAccount()
+        {
+            balance = 0;     
+        }
+
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine($"${balance}");
+        }
+    }
+}
---
+++ EncapsulationExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new BankAccount();
+            myAccount.Deposit(450);
+            myAccount.GetBalance();
+            myAccount.Deposit(375);
+            myAccount.GetBalance();
+
+        }
+    }
+}
